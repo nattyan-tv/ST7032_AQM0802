19b397124a23a806e10362b0a3021630277c9c68
Asuk Kono <asukiaaa@gmail.com>
2019-09-25 07:31:35 +0900

    Simplify example about HelloWorld and setWire

--- examples/setWire/setWire.ino
+++ examples/setWire/setWire.ino
@@ -1,57 +1,12 @@
-/*
-	ST7032 I2C LCD control sample
-	Original source : HelloWorld.ino (LiquidCrystal Library)
-	2013/05/21 tomozh@gmail.com
-*/
-
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
-
 #include <ST7032_asukiaaa.h>
 
 ST7032_asukiaaa lcd;
 
 void setup() {
-
 #if defined(ESP32)
-  Wire.begin(25, 26); // SDA, SCL
+  Wire.begin(21, 22); // SDA, SCL
   lcd.setWire(&Wire);
 #endif
-
   lcd.begin(16, 2);
   lcd.setContrast(40);
   lcd.print("hello, world!");
